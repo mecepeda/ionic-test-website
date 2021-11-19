77a41976585e630eaee25b3507ad90d51e43a267
Matías Ezequiel Cepeda <94414459+mecepeda@users.noreply.github.com>
2021-11-19 16:48:43 -0300

    Auto-generated commit [ci skip]

---
+++ assets/form-contact.ts
@@ -0,0 +1,15 @@
+export class FormContact {
+    public nombre:string
+    public apellido:string
+    public provincia:string
+    public localidad:string
+    public telefono:string
+    public email:string
+
+    /**
+     *
+     */
+    constructor() {
+        
+    }
+}
